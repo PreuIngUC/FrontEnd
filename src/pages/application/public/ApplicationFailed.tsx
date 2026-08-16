7c8d5c7cb58736eb27b1df6d512fbcf278f35656
polnomas <p.j.altamirano.p@gmail.com>
2026-08-16 10:11:12 -0400

    más texto explicativo en formulario de equipo

--- src/pages/application/public/ApplicationFailed.tsx
+++ src/pages/application/public/ApplicationFailed.tsx
@@ -1,7 +1,14 @@
 //TODO: estilos
 //TODO: ícono de equiz roja
 function ApplicationFailed() {
-  return <p>No pudimos enviar tu postulación</p>
+  return (
+    <>
+      <p>No pudimos enviar tu postulación, puedes notificarnos en:</p>
+      <a href="mailto:contacto@preuinguc.org" className="font-bold hover:underline">
+        contacto@preuinguc.org
+      </a>
+    </>
+  )
 }
 
 export default ApplicationFailed
